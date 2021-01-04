9f3c070c8f793b2084eb3e40f890b8833c482d13
n.mekh <ecvr813b>
2021-01-04 16:27:58 +0300

    HGCG-76: NumberUtils для дробного количества товаров

---
+++ src/esas/cmsgate/utils/NumberUtils.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace esas\cmsgate\utils;
+
+
+class NumberUtils
+{
+    /**
+     * @param $number
+     * @return bool
+     */
+    public static function hasDecimalPart($number) {
+        return self::getDecimalPart($number) > 0;
+    }
+
+    public static function getDecimalPart($number) {
+        if (!is_numeric($number))
+            return 0;
+        $pointPos = strpos($number, ".");
+        if ($pointPos <=0 )
+            return 0;
+        $decimalPart = substr($number, $pointPos + 1, strlen($number));
+        return intval($decimalPart);
+    }
+}
