44cc7191f86472f3bece4f63eb5a07509601cd77
nikita.mekh <n.mekh@hutkigrosh.by>
2022-02-03 14:35:37 +0300

    CMSGATE-49: CacheRepositoryPDOCrypto исравление по ключам

--- src/esas/cmsgate/cache/CacheRepositoryPDOCrypto.php
+++ src/esas/cmsgate/cache/CacheRepositoryPDOCrypto.php
@@ -5,26 +5,38 @@ namespace esas\cmsgate\cache;
 
 
 use Defuse\Crypto\Crypto;
+use Defuse\Crypto\Key;
 
 class CacheRepositoryPDOCrypto extends CacheRepositoryPDO
 {
-    private $cryptPassword;
+    private $key;
 
-    public function __construct($dsn, $user, $pass, $tableName, $cryptPassword)
+
+    public function __construct($dsn, $user, $pass, $tableName)
     {
         parent::__construct($dsn, $user, $pass, $tableName);
-        $this->cryptPassword = $cryptPassword;
+        if (file_exists($this->keyFileName())) {
+            $keyStr = file_get_contents($this->keyFileName());
+            $this->key = Key::loadFromAsciiSafeString($keyStr);
+        } else {
+            $this->key = Key::createNewRandomKey();
+            file_put_contents($this->keyFileName(), $this->key->saveToAsciiSafeString());
+        }
+    }
+
+    private function keyFileName() {
+        return (dirname(__FILE__)) . '/key.bin';
     }
 
 
     public function convertToDB($orderData)
     {
-        return Crypto::encrypt($orderData, $this->cryptPassword);
+        return Crypto::encrypt($orderData, $this->key);
     }
 
     public function convertFomDB($orderData)
     {
-        return Crypto::decrypt($orderData, $this->cryptPassword);
+        return Crypto::decrypt($orderData, $this->key);
     }
 
 
