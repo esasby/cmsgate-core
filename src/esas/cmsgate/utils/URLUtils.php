034cf8a14142663ae078da78cf29415d21a6a130
nikita.mekh <n.mekh@hutkigrosh.by>
2023-01-18 11:27:21 +0300

    CMSGATE-51: Исправление бага bitrix24 с накоплением дубликатов параметра user_lang при возврате на страницу оплаты товара

--- src/esas/cmsgate/utils/URLUtils.php
+++ src/esas/cmsgate/utils/URLUtils.php
@@ -18,4 +18,14 @@ class URLUtils
         $mainPart = "/" . StringUtils::substrBefore($_SERVER['REQUEST_URI'], "/", 1);
         return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$mainPart";
     }
+
+    public static function removeParamDuplicates($url) {
+        if (strpos($url, '?') <=0)
+            return $url;
+        $urlArray = explode('?', $url);
+        $mainPart = $urlArray[0];
+        $paramsPart = $urlArray[1];
+        $paramsArray = array_unique(explode('&', $paramsPart));
+        return $mainPart . '?' . implode('&', $paramsArray);
+    }
 }
